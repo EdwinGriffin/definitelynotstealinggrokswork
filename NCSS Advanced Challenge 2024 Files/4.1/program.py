93c79103f23feaec1b4aff363dfafb8fedd3f895
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2024-09-09 14:24:35 +1000

    Create program.py

---
+++ NCSS Advanced Challenge 2024 Files/4.1/program.py
@@ -0,0 +1,9 @@
+class Backpack
+  def __int__(self, brand, colour, capacity):
+    self.brand = brand
+    self-colour = colour
+    self.capacity = capacity
+
+if __name__ == '__main__':
+  my_backpack = Backpack('Billabong' 'Aqua' 18)
+  print(backpack.colour)
