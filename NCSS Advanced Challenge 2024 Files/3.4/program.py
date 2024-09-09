e94b5bfd5dce38f800b078424e249547eaed9c81
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2024-09-09 13:37:15 +1000

    Create program.py

---
+++ NCSS Advanced Challenge 2024 Files/3.4/program.py
@@ -0,0 +1,9 @@
+def is_palindrome(word):
+    #todo
+
+
+check_word = input("Enter the word(s) to check: ")
+if is_palindrome(check_word):
+  print("The input is a palindrome.")
+else:
+  print("The input is not a palindrome.")
